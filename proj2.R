d079cab0c4263c00d5d1248415f7424aca53615a
Yancy0517 <hjl000517@gmail.com>
2022-10-20 16:00:20 +0100

    Jialong part

--- proj2.R
+++ proj2.R
@@ -98,3 +98,38 @@ Pall(5, 3, 10000);Pall(50, 3, 10000)
 # of the other two strategies is approximately 0. So, we guess the probability 
 # of strategy 1 will approach to 0.3 as n increases and the other two strategies 
 # will approach to 0.
+
+dloop <- function(n, nreps){
+  # We use this function to create a matrix with nreps rows and 2n columns where 
+  # all numbers are initially set to 0, assuming that each row is a simulation. 
+  # Find the different loop depths that occur at least once in each simulation, 
+  # match these different depths to the column by their depth and replace 0s with 
+  # 1s. (For example, for a loop with a leisure depth of 3, we replace the 0 in 
+  # the third column with a 1). Finally, sum each column of the nerps row separately 
+  # and divide by npers to get 2n-vector of probabilities.
+  prob_matrix <- matrix(0, nrow = nreps, ncol = 2*n) 
+  for (i in 1:nreps) {
+    ncard <- sample(1:(2*n), 2*n) 
+    for (j in 1:(2*n)) {
+      box <- j
+      depth <- 1
+      while (depth <= 2*n) {
+        if(ncard[box] == j){
+          prob_matrix[i, depth] = 1
+          break
+        }else{
+          box = ncard[box]
+          depth = depth + 1
+        }
+      }
+    }
+  }
+  prob <- colSums(prob_matrix)/nreps
+  return(prob)
+}
+y <- dloop(50, 10000) 
+x <- 1:100 
+# Let each x correspond to a y, which means each loop length correspond to its 
+# probability, then generate a plot.
+plot(x, y, xlab = "Length of loop", ylab = "Probabilities", pch = 20,
+     main = "Probabilities of the length of each loop")
